cb27b887180eee95ed230c34ae5a3cf9b49eda48
zein <z.jendeya@gmail.com>
2020-10-21 14:05:20 +0300

    edits to code mistakes, added basic css for functionality

--- scripts/file.js
+++ scripts/file.js
@@ -3,68 +3,67 @@ let QsArray = ["firstQ", "secondQ", "thirdQ", "fourthQ", "fifthQ",
     "sixthQ", "seventhQ", "eighthQ", "ninthQ", "tenthQ"];
 
 let results = {
-    player =[],
-    score =[]
+    player: [],
+    score: []
 }
 //when "LEADER BOARD" is pressed
 function showLeaderBoard() { //finished
-    getElementById("leaderBoard").style.display = "block";
-    getElementById("startPage").style.display === "none";
-    getElementById("userName").style.display === "none";
-    getElementById("Questions").style.display === "none";
-    if (results[player] === undefined || results[player].length == 0) {
-        let message = document.createAttribute('p');
+    document.getElementById('leaderBoard').style.display = "block";
+    document.getElementById('startPage').style.display = "none";
+    document.getElementById('userName').style.display = "none";
+    document.getElementById('Questions').style.display = "none";
+    if (results.player === undefined || results.player.length == 0) {
+        let message = document.createElement('p');
         let info = document.createTextNode("No results to show!");
         message.appendChild(info);
-        getElementById("leaderBoard").appendChild(message);
+        document.getElementById('leaderBoard').appendChild(message);
     } else {
         for (let i = 0; i < results[player].length; i++) {
             let plRst = document.createElement('p');
             let plInfo = document.createTextNode(results.player[i] + " score is: " + results.score[i]);
             plRst.appendChild(plInfo);
-            getElementById("leaderBoard").appendChild(plRst);
+            document.getElementById('leaderBoard').appendChild(plRst);
         }
     }
 }
 
 //when "start quiz" is pressed
 function enterName() { //finished
-    getElementById("userName").style.display === "block";
-    getElementById("leaderBoard").style.display = "none";
-    getElementById("startPage").style.display === "none";
-    getElementById("Questions").style.display === "none";
+    document.getElementById('userName').style.display = "block";
+    document.getElementById('leaderBoard').style.display = "none";
+    document.getElementById('startPage').style.display = "none";
+    document.getElementById('Questions').style.display = "none";
 }
 //when "back" is pressed
 function goBack() { //finished
-    getElementById("leaderBoard").style.display = "none";
-    getElementById("startPage").style.display === "block";
-    getElementById("userName").style.display === "none";
-    getElementById("Questions").style.display === "none";
+    document.getElementById('leaderBoard').style.display = "none";
+    document.getElementById('startPage').style.display = "block";
+    document.getElementById('userName').style.display = "none";
+    document.getElementById('Questions').style.display = "none";
 }
 
 //to select a random question
 function selectRandomQ() { //finished
-    let randomOne;
-    let randomQ = randomOne.getElementById(QsArray[Math.floor(Math.random() * QsArray.length)]);
+    let randomQ = document.getElementById(QsArray[Math.floor(Math.random() * QsArray.length)]);
     return randomQ;
 }
 
 
 //when "submit" in entering name page is pressed
 function startQuiz() { //unfinished
-    getElementById("userName").style.display === "block";
-    getElementById("leaderBoard").style.display = "none";
-    getElementById("startPage").style.display === "none";
-    getElementById("Questions").style.display === "none";
-    let playerName = getElementById("name").value;
+    document.getElementById('userName').style.display = "block";
+    document.getElementById('leaderBoard').style.display = "none";
+    document.getElementById('startPage').style.display = "none";
+    document.getElementById('Questions').style.display = "none";
+    let playerName = document.getElementById('name').value;
     if (playerName.length == 0) {
         alert("you must enter a name!");
     } else {
-        results[player].push(playerName);
-        getElementById("leaderBoard").style.display = "none";
-        getElementById("startPage").style.display === "none";
-        getElementById("userName").style.display === "none";
-        getElementById("Questions").style.display === "block";
+        results.player.push(playerName);
+        document.getElementById('leaderBoard').style.display = "none";
+        document.getElementById('startPage').style.display = "none";
+        document.getElementById('userName').style.display = "none";
+        document.getElementById('Questions').style.display = "block";
         let question1 = selectRandomQ();
         //make this only question appear and hide others
         question1.style.display === "block";
@@ -80,7 +79,7 @@ function nextQ() { //unfinished
         //select random question
         let Qs = selectRandomQ();
         //show selected random question
-        Qs.style.display === "block";
+        Qs.style.display = "block";
         //show question's number
         let QNumberView = document.createElement('p');
         let QNumber = document.createTextNode(counter + "/10");
@@ -96,16 +95,16 @@ function nextQ() { //unfinished
         //store it in results score array
         results[score].push(sumOfResult);
         //hide questions div
-        getElementById("Questions").style.display === "none";
+        document.getElementById('Questions').style.display = "none";
         //show leader board
-        getElementById("leaderBoard").style.display = "block";
-        getElementById("startPage").style.display === "none";
-        getElementById("userName").style.display === "none";
+        document.getElementById('leaderBoard').style.display = "block";
+        document.getElementById('startPage').style.display = "none";
+        document.getElementById('userName').style.display = "none";
         for (let i = 0; i < results[player].length; i++) {
             let plRst = document.createElement('p');
             let plInfo = document.createTextNode(results.player[i] + " score is: " + results.score[i]);
             plRst.appendChild(plInfo);
-            getElementById("leaderBoard").appendChild(plRst);
+            document.getElementById('leaderBoard').appendChild(plRst);
         }
     }
 }
